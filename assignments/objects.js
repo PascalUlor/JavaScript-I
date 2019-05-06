3ba14f4e7c4c412160f9ce45d38de1bc279db349
PascalUlor <pascalulor@yahoo.com>
2019-05-06 18:01:37 +0100

    ft(add speak method to kennan object)

--- assignments/objects.js
+++ assignments/objects.js
@@ -29,7 +29,8 @@ const interns = [
     id: '2',
     name: 'Kennan',
     email: 'kdiben1@tinypic.com',
-    gender: 'M'
+    gender: 'M',
+    speak: ()=> 'Hello, my name is Kennan!'
   },
   {
     id: '3',
@@ -74,6 +75,7 @@ console.log(interns[4].gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+console.log(interns[1].speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
