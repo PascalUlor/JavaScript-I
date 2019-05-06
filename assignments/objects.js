93bc676d191ae6695ff0914f2aabda93e1a1d1c3
PascalUlor <pascalulor@yahoo.com>
2019-05-06 19:37:22 +0100

    ft(create nested object) add speak method using this

--- assignments/objects.js
+++ assignments/objects.js
@@ -48,7 +48,8 @@ const interns = [
     id: '5',
     name: 'Antonietta',
     email: 'adaine5@samsung.com',
-    gender: 'F'
+    gender: 'F',
+    multiplyNums: (a,b) => a*b
   }
 ]
 // Display Database Table
@@ -79,6 +80,7 @@ console.log(interns[1].speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+console.log(interns[4].multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -89,16 +91,36 @@ console.log(interns[1].speak());
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: 'Susan',
+  age: 70,
+  speak: function(){ return 'Hello, my name is ' + this.name},
+  child:{
+   name: 'George',
+   age: '50',
+   speak: function(){ return 'Hello, my name is ' + this.name},
+   grandChild: {
+     name: 'Sam',
+     age: 30,
+     speak: function(){ return 'Hello, my name is ' + this.name}
+   }
+  }
+}
 
 // Log the parent object's name
+console.table(parent);
+console.log(parent.name);
 
 // Log the child's age
+console.log(parent.child.name);
 
 // Log the name and age of the grandchild
+console.log(parent.child.grandChild.name);
 
 // Have the parent speak
-
+console.log(parent.speak());
 // Have the child speak
+console.log(parent.child.speak());
 
 // Have the grandchild speak
+console.log(parent.child.grandChild.speak());
